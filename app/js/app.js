c1777540134b5b58001fb2db2d82d8079dbd45e2
JoshuaWhalen <joshuawhalen1994@gmail.com>
2019-02-02 21:56:25 -0500

    sw test

--- app/js/app.js
+++ app/js/app.js
@@ -18,7 +18,7 @@ document.addEventListener('click', clickEffect);
 
 if('serviceWorker' in navigator) {
   navigator.serviceWorker
-           .register('../sw.js')
+           .register('/js/sw.js', { scope: '/' })
            .then(function() { console.log('Service Worker Registered'); });
 }
 
