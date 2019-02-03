811f330e40508be66ff79283c8968dc7d2636f5e
JoshuaWhalen <joshuawhalen1994@gmail.com>
2019-02-02 23:59:46 -0500

    style PWA install button

--- app/js/app.js
+++ app/js/app.js
@@ -30,7 +30,7 @@ addBtn.style.display = 'none';
 
 window.addEventListener('beforeinstallprompt', (e) => {
   // Prevent Chrome 67 and earlier from automatically showing the prompt
-  //e.preventDefault();
+  e.preventDefault();
   // Stash the event so it can be triggered later.
   deferredPrompt = e;
   // Update UI to notify the user they can add to home screen
